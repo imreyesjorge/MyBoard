c9943798a7315cccb8ef6513bd41a4e889ab96f9
Jorge Reyes <reyestrevjorge@gmail.com>
2021-05-12 00:33:26 -0500

    :sparkles: feat: Add State for the Input
    Added the state controller for the tag searcher input

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 import Layout from "../src/components/Layout";
 import Nav from "../src/components/Nav";
 
 const index = () => {
+  const [tag, setTag] = useState("");
+
   return (
     <Layout>
-      <Nav />
+      <Nav searched={tag} setSearched={setTag} />
     </Layout>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
-const Nav = () => {
+const Nav = ({ searched, setSearched }) => {
+  const handleChange = (event) => {
+    setSearched(event.target.value);
+  };
+
   return (
     <nav className="w-full p-8 flex justify-between items-center">
       <div className="flex justify-center items-center">
@@ -8,12 +12,18 @@ const Nav = () => {
         <h3 className="text-lg ml-4">WorkspaceName</h3>
       </div>
       <div className="flex justify-center items-center">
-        <div className="flex mr-6 p-3 px-4 items-center justify-center bg-backalt rounded-lg">
-          <input type="text" className="font-semibold bg-transparent" />
+        <div className="flex mr-6 p-2 px-3 items-center justify-center bg-backalt rounded-lg">
+          <input
+            type="text"
+            placeholder="Search by Tag-name"
+            value={searched}
+            onChange={handleChange}
+            className="font-semibold bg-transparent"
+          />
           <Image src="/svgs/Search.svg" width={25} height={25} />
         </div>
         <div className="flex items-center justify-center cursor-pointer">
-          <Image src="/svgs/Edit Square.svg" width={48} height={48} />
+          <Image src="/svgs/Edit Square.svg" width={40} height={40} />
         </div>
       </div>
     </nav>
